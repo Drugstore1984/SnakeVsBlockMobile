bd19a8e15e5458b4126229a0c294f04f35f45b0c
Drugstore1984 <47272821+Drugstore1984@users.noreply.github.com>
2022-09-18 23:21:12 +0300

    Add pause.

--- Assets/Scripts/Game/Pause.cs
+++ Assets/Scripts/Game/Pause.cs
@@ -1,13 +1,17 @@
-
 using UnityEngine;
-
 public class Pause : MonoBehaviour
 {
     private SnakeMove snakeMove;
+    [SerializeField] private GameObject _pauseMenu;
+    private void OnApplicationPause()
+    {
+        PauseGame();
+    }
     public void PauseGame()
     {
         snakeMove = FindObjectOfType<SnakeMove>();
         snakeMove.SnakeStop();
+        _pauseMenu.SetActive(true);
     }
     public void ResumeGame()
     {
