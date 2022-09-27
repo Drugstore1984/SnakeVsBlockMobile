fe691f7ec2c3e51de3873ea06a0c8a0c4b1b5383
Drugstore1984 <47272821+Drugstore1984@users.noreply.github.com>
2022-09-27 23:38:53 +0300

    Create helath move prefab.

--- Assets/Scripts/Enviroment/EnviromentMove.cs
+++ Assets/Scripts/Enviroment/EnviromentMove.cs
@@ -1,6 +1,42 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class EnviromentMove : MonoBehaviour
 {
-
+    [SerializeField] private float _speed;
+    [SerializeField] private float _xMin,_xMax;
+    [SerializeField] private bool _isMovingRight = true;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+        Flip();
+    }
+    private void Move()
+    {
+        if (_isMovingRight)
+        {
+            rb.velocity = new Vector3(_speed, 0, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector3(_speed * -1, 0, 0);
+        }
+    }
+    private void Flip()
+    {
+        if (transform.position.x <= _xMin)
+        {
+            _isMovingRight = true;
+        }
+        else if(transform.position.x >= _xMax)
+        {
+            _isMovingRight = false;
+        }
+    }
 }
